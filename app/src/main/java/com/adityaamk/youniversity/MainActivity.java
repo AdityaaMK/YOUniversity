993383e733c14b508d251e0179b79bd96b57139f
AdityaaMK <44482076+AdityaaMK@users.noreply.github.com>
2020-04-04 15:55:34 -0400

    Almost finished Search Fragment.

--- app/src/main/java/com/adityaamk/youniversity/MainActivity.java
+++ app/src/main/java/com/adityaamk/youniversity/MainActivity.java
@@ -9,29 +9,31 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    ArrayList<University> universities;
+    Fragment selectedFragment = new HomeFragment();
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        Fragment selectedFragment = null;
 
         switch (menuItem.getItemId()) {
             case R.id.navigation_home:
                 selectedFragment = new HomeFragment();
                 break;
-
-//            case R.id.navigation_rankings:
-//                fragment = rFrag;
-//                break;
-//
-//            case R.id.navigation_search:
-//                fragment = searchFrag;
-//                break;
-//
-//            case R.id.navigation_fit:
-//                fragment = fFrag;
-//                break;
+            case R.id.navigation_search:
+                selectedFragment = new SearchFragment();
+                break;
+            case R.id.navigation_list:
+                selectedFragment = new ListFragment();
+                break;
+            case R.id.navigation_target:
+                selectedFragment = new GraphFragment();
+                break;
         }
+
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
         return true;
     }
@@ -40,6 +42,10 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+        universities = new ArrayList<>();
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(this);
     }
